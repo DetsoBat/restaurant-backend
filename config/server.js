a134151ef842893a017051143fa8a7b7c500e72b
DetsoBat <Delgertsogt9992@gmail.com>
2022-08-21 01:28:20 -0500

    init

--- config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'd92a480ff01338c21efc6ad24f2a67f9'),
-    },
-  },
-});
--- extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'b9372e6d-bf89-4c38-8afd-475500163096'
-};
